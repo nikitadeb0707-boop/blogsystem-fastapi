134d3ebe86860c35bdb494f1d97717ac5ab29022
Nikita Deb <nikitadeb.0707@gmail.com>
2026-08-07 21:19:01 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:nikita2007@localhost:5432/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}'
 engine= create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(
     autocommit= False,
